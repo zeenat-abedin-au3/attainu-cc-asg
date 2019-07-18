387e1e3f7001d4b286a5076d2aec9602ed8095bd
zeenat-abedin-au3 <zeenu.23@gmail.com>
2019-07-19 00:44:57 +0530

    week03day03 assignment complete

---
+++ assignments/week03/day03/script.js
@@ -0,0 +1,7 @@
+document.querySelector("div").addEventListener("mouseover", function() {
+    document.body.style.backgroundColor = "pink";
+});
+
+document.querySelector("div").addEventListener("mouseout", function() {
+    document.body.style.backgroundColor = "white";
+});

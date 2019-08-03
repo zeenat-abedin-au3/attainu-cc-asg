7e9681c4833257673090105678556c8408fac202
zeenat-abedin-au3 <zeenu.23@gmail.com>
2019-08-04 02:55:55 +0530

    week05day05 cc done

--- coding-challenges/week05/day05/params2.js
+++ coding-challenges/week05/day05/params2.js
@@ -12,9 +12,9 @@ function params(url) {
 
     for (var i = 0; i < url.length; i++) {
         var char = url[i]; // storing the current character
-        //checks for keys
+        //check for Keys
         if(char == '?' || char == '&' || readKeys == true) { //clues
-            if(readKeys == false) {
+            if(readKeys == false) { //key starts
                readKeys = true
                keyStart = i + 1
             }
@@ -25,8 +25,32 @@ function params(url) {
             }
         }
 
+    return keysArray
+
+        //check for Values
+        if(char == '=' || readValues == true) { //clues
+            if(readValues== false) { //value starts
+               readValues = true
+               valueStart = i + 1
+            }
+            if(char == '&') { // value ends
+                valueEnd = i
+                valuesArray.push( url.substring(valueStart, valueEnd)) //doesn't include the ending index
+                readValues = false
+            }else if(i == url.length - 1)
+                valueEnd = i + 1
+                valuesArray.push( url.substring(valueStart, valueEnd)) //doesn't include the ending index
+                readValues = false
+            }
+        
+        }
+        
     }
 
-    return keysArray
+    for(var i = 0; i < keysArray.length; i++){
+        paramsObj[keysArray[i]] = valuesArray[i] //creating the object
+    }
+    return paramsObj
 }
+
 console.log(params("http://localhost:3000/add?num1=5&num2=3"))

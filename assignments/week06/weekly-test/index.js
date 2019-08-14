4dd4544efa560d909b7442a729bdd3c99d66916e
zeenat-abedin-au3 <zeenu.23@gmail.com>
2019-08-14 14:45:38 +0530

    week06 test done

--- assignments/week06/weekly-test/index.js
+++ assignments/week06/weekly-test/index.js
@@ -1,92 +1,52 @@
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var users = require('./users.json');
+var tweets = require('./tweets.json');
 var hbs = require('hbs')
 
 var app = express();
 
 app.use(session({
-    secret: 'This is my session ID cookie'
+    secret: 'secret for login page'
 }));
 
 app.use(bodyParser.urlencoded());
+app.set('view engine', 'hbs');
+    res.render('tweet.hbs');
+
 
 app.use(express.static('public'));
-
-//view engine
-app.set('views', __dirname + '/views');
-app.set('view engine', 'hbs');
-app.engine('hbs', '/tweet.hbs' );
-
-
-var users = [
-    {
-        username: "digitizer",
-        password: "digitizer"
-    },
-    {
-        username: "mosh",
-        password: "hello123"
-    }
-]
-
-var tweets=[
-    {
-        name: "digitizer",
-        tweet: "This is a cool piece of information."
-    },
-    {
-        name: "M0SH",
-        tweet: "@digitizer I agree."
-    }
-]
-
-
-app.use(function(req, res, next) {
-    if(!req.session.views) {
-        req.session.views = {};
-    }
-    var path = req.originalUrl;
-    if(req.session.views[path] > 0)
-        req.session.views[path]++;
-    else
-        req.session.views[path] = 1;
-    next();
-});
-
-app.get('/', function (req, res) {
-    if (req.session.loggedIn)
-        res.redirect('/profile');
-    else
-        // res.sendFile(__dirname + '/public/index.html');
-        res.redirect('/index.html');
-});
-
-app.get('/profile', function (req, res) {
-    if(req.session.loggedIn)
-        res.send("<h1>Welcome " + req.session.user.username + "</h1><h2>You are visiting here for the " + req.session.views[req.originalUrl] + " time</h2><a href='/logout'>Log out</a>");
-    else
-        res.redirect('/');
+app.get('/login', function(req, res){
+res.sendfile(__dirname +'/public/index.html');
+    
 });
 
 app.post('/auth', function(req, res) {
-    var flag = false;
-    for(var i=0; i<users.length; i++) {
-        if (users[i].username == req.body.username && users[i].password == req.body.password) {
-            flag = true;
-            break;
+    var flag = false
+    for(var i=0; i<users.length; i++){
+     if (req.body.username == users[i].name && req.body.password == users[i].password)
+        {
+         flag = true;
+        break;
         }
     }
     if(flag) {
         req.session.loggedIn = true;
-        req.session.user = {
-            username: "digitizer",
-        };
-        res.redirect("/profile");
+        res.redirect('/user');
+    }else {
+        res.redirect('/login');
     }
-    else
-        res.redirect("/");
 });
+
+app.get('/user', function(req, res) {
+    if (req.session.loggedIn == true) {
+        res.sendfile('./index.html');
+    } else {
+        res.redirect('/');
+    }
+});
+
 
 app.get('/logout', function(req, res) {
     req.session.destroy();

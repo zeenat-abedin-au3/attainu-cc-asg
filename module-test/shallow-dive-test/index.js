f06f2299f7ea675be3f447af396acbc65debc668
zeenat-abedin-au3 <zeenu.23@gmail.com>
2019-09-26 18:24:42 +0530

    fixed issue

--- module-test/shallow-dive-test/index.js
+++ module-test/shallow-dive-test/index.js
@@ -4,7 +4,7 @@ var session = require("express-session");
 var mongoClient = require("mongodb").MongoClient;
 var db;
 
-mongoClient.connect( 'mongodb://localhost:27017', function (err, client) {
+mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
     if (err) throw err;
     db = client.db("login");
 });
@@ -13,7 +13,9 @@ mongoClient.connect( 'mongodb://localhost:27017', function (err, client) {
 var app = express();
 app.use(
     session({
-        secret: "Express session secret"
+        secret: "Express session secret",
+        resave: false,
+        saveUninitialized: true,
     })
 );
 
@@ -31,25 +33,64 @@ app.use(express.urlencoded({
     extended: false
 }));
 
+app.get("/", (req, res) => {
+    res.redirect("/login")
+})
 
+app.get("/dashboard", (req, res) => {
+    if (!req.session.loggedin) {
+        res.redirect('/login')
+        return
+    }
+    res.render("dashboard", {
+        title: "Dashboard Page"
+    })
+})
+
+app.get("/login", (req, res) => {
+    res.render("login", {
+        title: "Login"
+    })
+})
+app.post('/login', (req, res) => {
+    var { email, password } = req.body
+    db.collection("users").findOne({ email: email }, function (err, result) {
+        if (err) throw err;
+
+        if (!result) {
+            res.redirect("/login")
+            return
+        }
+        if (result.password !== password) {
+            res.redirect("/login")
+            return
+        }
+        req.session.loggedin = true
+        res.redirect("/dashboard")
+    })
+})
 app.get("/signup", function (req, res) {
     res.render("signup", {
         title: "add Users",
         user: req.session.user
     });
 });
+app.post("/signup", function (req, res) {
+    var { username, email, mobile, password } = req.body
 
-
-app.post("/users/user", function (req, res) {
-    db.collection("users").insertOne(req.body, function (err, result) {
+    db.collection("user").insertOne({ username, email, mobile, password }, function (err, result) {
         if (err) throw err;
-        console.log(req.body)
         res.redirect("/login")
     })
-});
+})
 
 
-app.listen(port, function (req, res) {
+app.get("/logout", (req, res) => {
+    req.session.destroy()
+    res.redirect("/")
+})
+
+app.listen(3000, function (req, res) {
     console.log("listening at 3000");
 });
 

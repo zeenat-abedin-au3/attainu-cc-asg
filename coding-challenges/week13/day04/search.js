6a53a5ed8a2d0bd1ab4a4c51a62242174fcbfbbc
zeenat-abedin-au3 <zeenu.23@gmail.com>
2019-10-03 00:01:26 +0530

    fixed search.js

--- coding-challenges/week13/day04/search.js
+++ coding-challenges/week13/day04/search.js
@@ -2,17 +2,16 @@ const fs = require('fs')
 
 function search(filename, keyword) {
 
-    fs.readFile(filename, 'utf-8', function (error, data) { //read data
+    let data = fs.readFileSync(filename, 'utf-8') //read data
 
-        let lines = data.split('\n') //split into lines in the form of an array
-        for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
+    let lines = data.split('\n') //split into lines in the form of an array
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
 
-            if (line.indexOf(keyword) != -1) console.log(`line: ${i + 1} data: ${line}`)
-        }
-
-    })
+        if (line.indexOf(keyword) != -1) console.log(`line: ${i + 1} data: ${line}`)
+    }
+    return 'not found!'
 
 }
 
-search("text.txt", "Thursday")
+console.log(search("text.txt", "Friday"))

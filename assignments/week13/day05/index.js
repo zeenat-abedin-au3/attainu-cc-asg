7f161ce4e0bf186036415858c879b42aafaef322
zeenat-abedin-au3 <zeenu.23@gmail.com>
2019-10-04 11:31:12 +0530

    week13day05 assignment done

---
+++ assignments/week13/day05/index.js
@@ -0,0 +1,6 @@
+const sum = (num1, num2) => num1 + num2;
+
+const power = (num, exp) => num ** exp;
+
+console.log(sum(2, 4));
+console.log(power(2, 3));
---
+++ assignments/week13/day05/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var sum = function sum(num1, num2) {
+  return num1 + num2;
+};
+
+var power = function power(num, exp) {
+  return Math.pow(num, exp);
+};
+
+console.log(sum(2, 4));
+console.log(power(2, 3));

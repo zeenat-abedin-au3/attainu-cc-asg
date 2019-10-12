dda8a42a038eb0c3af0f699e009f48be3e642f15
zeenat-abedin-au3 <zeenu.23@gmail.com>
2019-10-12 11:26:31 +0530

    week15day03 assignment done

---
+++ assignments/week15/day03/index.js
@@ -0,0 +1,18 @@
+const selectionSort = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    return arr;
+};
+
+console.log(selectionSort([4, 8, 3, 6, 15, 45, 32, 1]));
